cad7f0e77f7669841f4892d3b7cf703c97f8d461
Djordje Knezevic <starwarrior@mycity.rs>
2023-02-27 12:05:02 +0100

    Initial Commit

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by Djordje Knezevic on 27.2.23..
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
